4c9343230f3af09964614e75cb6cac496990ce40
JovankaN <jovankanikolovski@gmail.com>
2016-08-05 13:22:22 -0500

    some additions

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   get 'static_pages/index'
 
-  get 'static_page/about'
 
-  get 'static_page/contact'
+
 
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
-  get 'static_page/index'
 
-  root 'static_page#index'
+
+  root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
