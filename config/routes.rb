5f817421193c81fd14f9ee97e9f9bfa494697df5
JovankaN <jovankanikolovski@gmail.com>
2016-08-10 18:41:05 -0500

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   resources :products
-
   resources :orders, only: [:index, :show, :create, :destroy]
 
 
 
 
-
+  get 'static_pages/index'
   get 'static_pages/about'
 
   get 'static_pages/contact'
