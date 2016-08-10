24f0e736278806df48e60aa280d9fd8182246e9d
JovankaN <jovankanikolovski@gmail.com>
2016-08-10 18:34:20 -0500

    4.3 trying to fix nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :products
-  get 'static_pages/index'
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
 
 
 
@@ -13,7 +15,6 @@ Rails.application.routes.draw do
 
   root 'static_pages#landing_page'
 
-resources :orders, only: [:index, :show, :create, :destroy]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
