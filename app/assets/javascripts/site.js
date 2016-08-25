90b140e78295f7837204bc48802939f0305f722a
JovankaN <jovankanikolovski@gmail.com>
2016-08-25 01:58:01 -0500

    updates

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,7 @@
 // this file will contain any site-wide functions
 
 $(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]'  });
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });//specified where the image assets are found
     $('.rated').raty({ path: '/assets',
       readOnly: true,
       score: function() {
