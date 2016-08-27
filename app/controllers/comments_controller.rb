808b3e16ce42dfc11770ddca75c2c0c694fa10f9
JovankaN <jovankanikolovski@gmail.com>
2016-08-27 00:33:04 -0500

    update destroy function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,7 @@ class CommentsController < ApplicationController
 
   def destroy
 
-    @comment = Comment.find(params[:comment_id])
+    @comment = Comment.find(params[:id])
     product = @comment.product
     @comment.destroy
     redirect_to product
