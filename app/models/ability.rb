e58d82db4f85fd13e0bf6c9d2db3fad60a7bd172
JovankaN <jovankanikolovski@gmail.com>
2016-08-26 22:03:34 -0500

    update destroy function

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,9 @@ class Ability
     if user.admin?
       can :manage, :all
     else
-      can :read, :all
       can :manage, User, id: user.id
       cannot :destroy, Comment
+      can :read, :all
     end
 
   end
