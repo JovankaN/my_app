407d9dcdcb030967a24ebd864929ceb08db02664
JovankaN <jovankanikolovski@gmail.com>
2016-09-07 15:18:02 -0700

    6.2 unit tests update

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe Product do
 
   before do
-    @product = Product.create!(name: "race bike")
+    @product = Product.create!(name: "Sand dunes")
     @user = User.create!(first_name: "Jovanka", last_name: "Someone", email: "test@something.com", password: "123456")
-    @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
-    @product.comments.create!(rating: 3, user: @user, body: "Amazing bike!")
-    @product.comments.create!(rating: 5, user: @user, body: "Beautiful bike!")
+    @product.comments.create!(rating: 1, user: @user, body: "Awful picture!")
+    @product.comments.create!(rating: 3, user: @user, body: "Amazing picture!")
+    @product.comments.create!(rating: 5, user: @user, body: "Beautiful picture!")
   end
 
   it "returns the average rating of all comments" do
@@ -15,7 +15,7 @@ describe Product do
   end
 
   it "iis not valid" do
-    expect(Product.new(description: "Nice bike")).not_to be_valid
+    expect(Product.new(description: "Nice view")).not_to be_valid
   end
 
 
