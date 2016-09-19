e614d10aa58d23dfcfe7576ad9a868a676d6ff9f
JovankaN <jovankanikolovski@gmail.com>
2016-09-19 18:40:09 -0500

    6.9 jquery pluggin added

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,7 @@
 // this file will contain any site-wide functions
 
 $(document).on('turbolinks:load', function(){
+    $('.img-zoom').elevateZoom();
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });//specified where the image assets are found
     $('.rated').raty({ path: '/assets',
       readOnly: true,
