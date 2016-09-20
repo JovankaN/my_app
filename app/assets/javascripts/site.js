93dc893ba5856f52e1911aa056806e899d8455c3
JovankaN <jovankanikolovski@gmail.com>
2016-09-20 02:04:26 -0500

    6.10 updated

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,18 +1,19 @@
 // this file will contain any site-wide functions
 
-var refreshRating= function() {
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/assets',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
     });
 };
 
-$(document).on('turbolinks:load ajaxSuccess', function(){
-  refreshRating();
-  $('.img-zoom').elevateZoom();
+$(document).on('turbolinks:load', function() {
+    refreshRating();
+
+    $('.img-zoom').elevateZoom();
 });
 
 
