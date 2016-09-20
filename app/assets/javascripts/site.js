2c8b8eff139f3edf0b197710972ceabf2cff054d
JovankaN <jovankanikolovski@gmail.com>
2016-09-20 00:39:29 -0500

    6.10 Ajax

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,18 @@
 // this file will contain any site-wide functions
 
-$(document).on('turbolinks:load', function(){
-    $('.img-zoom').elevateZoom();
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });//specified where the image assets are found
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
     });
+};
+
+$(document).on('turbolinks:load', function() {
+    refreshRating();
+
+    $('.img-zoom').elevateZoom();
 });
+
