5c007fa66e5f50a0c699eefde6d35f5c1575d768
JovankaN <jovankanikolovski@gmail.com>
2016-09-20 23:21:52 -0500

    6.9 6.10 updated

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -2,7 +2,7 @@
 
 
 $(document).on('turbolinks:load', function(){
-    $('.img-zoom').elevateZoom();
+    $(".img-zoom").elevateZoom({width: 400});
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });//specified where the image assets are found
     $('.rated').raty({ path: '/assets',
       readOnly: true,
