a214515f3645c76c1493abc67fb8740e22bce307
JovankaN <jovankanikolovski@gmail.com>
2016-09-24 01:09:55 -0500

    6.14 ActiveJob

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -1,10 +1,15 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class ProductChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "product_channel"
+    # stream_from "product_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def listen
+    stop_all_streams["product_id"]
+  end
+
 end
